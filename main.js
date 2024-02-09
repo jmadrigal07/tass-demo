2294111e31ecf8eabbe6d6bd847ddc85b8ba9303
jmadrigal07 <jmadrigal.webmaster@gmail.com>
2024-02-09 16:01:00 -0500

    feat: add sumar function

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 // Definición de una función
 function saludar(nombre) {
     console.log("¡Hola, " + nombre + "!");
-  }
+}
+
+function sumar(a, b) {
+    console.log(a+b)
+}
   
-  // Llamada a la función
-  saludar("Mundo");
-  
+// Llamada a la función
+saludar("Mundo");
+
+// Llamar Sumar
+sumar(10,20);
